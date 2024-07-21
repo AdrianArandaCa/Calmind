c616dae0f2d1594eb989324723b60073d0ca4983
Adrian AC <aacampanario91@gmail.com>
2024-07-21 16:31:55 +0200

    Initial Commit

---
+++ Calmind/CalmindApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalmindApp.swift
+//  Calmind
+//
+//  Created by Adrian Aranda Campanario on 21/7/24.
+//
+
+import SwiftUI
+
+@main
+struct CalmindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calmind/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calmind
+//
+//  Created by Adrian Aranda Campanario on 21/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
